04e72dc85bf4aaacd4611ab41fae5dcd21aa451d
malevka <formaleva@gmail.com>
2021-02-21 09:20:07 +0300

    refactor gulp config

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -1,37 +1,17 @@
 import { buildHtml, watchPugFiles } from "./build-html";
 import { buildStyles, watchStylesFiles } from "./build-styles";
-import svgSprite from "gulp-svg-sprite";
+import { buildSvgSprites } from "./build-svg-sprite";
+import { buildScripts } from "./build-scripts";
 import gulp from "gulp";
-import concat from "gulp-concat";
 
 const watch = () => {
   watchPugFiles();
   watchStylesFiles();
 };
-const config = {
-  mode: {
-    symbol: {
-      dest: "icons",
-      sprite: "sprite.symbol.svg"
-    }
-  }
-};
 
-const scripts = () => {
-  return gulp.src(["node_modules/jquery/dist/jquery.slim.min.js", "node_modules/popper.js/dist/umd/popper.min.js","node_modules/bootstrap/dist/js/bootstrap.min.js "])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('build/js/'));
-};
+const build = gulp.parallel(buildSvgSprites, buildScripts, buildHtml, buildStyles);
 
-const buildSvgSprites = () => {
-  return gulp.src("app/images/icons/*.svg")
-    .pipe(svgSprite(config))
-    .pipe(gulp.dest("build/images/"));
-};
-
-const build = () => gulp.parallel(buildHtml, buildStyles);
-
-export { buildHtml, build, watch, buildStyles, buildSvgSprites , scripts};
+export { buildHtml, build, watch, buildStyles, buildSvgSprites , buildScripts};
 
 
 
